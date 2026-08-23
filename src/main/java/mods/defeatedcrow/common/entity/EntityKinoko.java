aa9f5e21a365afb6e821ab5394eb02ccf803af7b
rubnsn <whiteruby.0214@gmail.com>
2026-08-24 08:03:21 +0900

    feat(WT-B): P3/P4/P5 wiring - BlockEntity real blocks, Fluid block linkage, EntityType full 20 (plan 10)

--- src/main/java/mods/defeatedcrow/common/entity/EntityKinoko.java
+++ src/main/java/mods/defeatedcrow/common/entity/EntityKinoko.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.Level;
 
 public class EntityKinoko extends Mob {
-    protected EntityKinoko(EntityType<? extends Mob> type, Level level) { super(type, level); }
-    public EntityKinoko(Level level, double x, double y, double z) { super((EntityType)mods.defeatedcrow.common.registry.ModEntities.PLACEABLE_ALCOHOL_CUP.get(), level); this.setPos(x,y,z); }
+    public EntityKinoko(EntityType<? extends Mob> type, Level level) { super(type, level); }
+    public EntityKinoko(Level level, double x, double y, double z) { super((EntityType)mods.defeatedcrow.common.registry.ModEntities.KINOKO.get(), level); this.setPos(x,y,z); }
+    @Override protected void defineSynchedData() { super.defineSynchedData(); }
 }
--- src/main/java/mods/defeatedcrow/common/entity/EntityYuzuBullet.java
+++ src/main/java/mods/defeatedcrow/common/entity/EntityYuzuBullet.java
@@ -1,2 +1,44 @@
 package mods.defeatedcrow.common.entity;
-public class EntityYuzuBullet {}
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+
+/**
+ * 1.20.1 stub for EntityYuzuBullet - projectile.
+ * Original 1.7.10 used motionX, blockX, etc.
+ * See doc/entities/migration-guide.md
+ */
+public class EntityYuzuBullet extends Entity {
+
+    public EntityYuzuBullet(EntityType<?> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
+    protected void defineSynchedData() {}
+
+    @Override
+    protected void readAdditionalSaveData(CompoundTag tag) {}
+
+    @Override
+    protected void addAdditionalSaveData(CompoundTag tag) {}
+
+    @Override
+    public Packet<ClientGamePacketListener> getAddEntityPacket() {
+        return super.getAddEntityPacket();
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (!this.level().isClientSide) {
+            this.setDeltaMovement(this.getDeltaMovement().add(0, -0.05, 0));
+            this.move(net.minecraft.world.entity.MoverType.SELF, this.getDeltaMovement());
+            if (this.tickCount > 100) this.discard();
+        }
+    }
+}
