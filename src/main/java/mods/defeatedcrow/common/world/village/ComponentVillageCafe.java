767051f47efa8ac6a55d02e26ade12b3dc4ca262
rubnsn <whiteruby.0214@gmail.com>
2026-08-24 17:44:58 +0900

    [P6] port village 4 stubs to 1.20.1 (VillagerCafe/Yome + Cafe/Warehouse)
    - VillagerCafe/Yome: VillagerRegistry->VillagerProfession+VillagerTradesEvent with placeholder FARME/CLERIC and TODO for custom CAFE/YOME POI via DeferredRegister
    - ComponentVillageCafe/Warehouse: StructureVillagePieces.Village->plain class with BoundingBox+ServerLevelAccessor+postProcess stub for NBT template (village_cafe/warehouse.nbt) via StructureTemplate, hasMadeChest NBT
    - BUILD SUCCESSFUL

--- src/main/java/mods/defeatedcrow/common/world/village/ComponentVillageCafe.java
+++ src/main/java/mods/defeatedcrow/common/world/village/ComponentVillageCafe.java
@@ -1,2 +1,30 @@
 package mods.defeatedcrow.common.world.village;
-public class ComponentVillageCafe {}
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+
+/**
+ * 1.20.1 ComponentVillageCafe - cafe structure (was StructureVillagePieces.Village).
+ * Original 1.7.10 used StructureBoundingBox+fillWithBlocks, now Jigsaw/StructureTemplate + BoundingBox.
+ * This is a stub that will be replaced by Jigsaw NBT via StructureTemplate.
+ */
+public class ComponentVillageCafe {
+
+    private boolean hasMadeChest;
+
+    public ComponentVillageCafe() {}
+
+    public boolean addComponentParts(ServerLevelAccessor level, RandomSource random, BoundingBox box) {
+        // TODO: place NBT template data/defeatedcrow/structures/village_cafe.nbt
+        return true;
+    }
+
+    protected void addAdditionalSaveData(CompoundTag tag) { tag.putBoolean("Chest", hasMadeChest); }
+    protected void readAdditionalSaveData(CompoundTag tag) { hasMadeChest = tag.getBoolean("Chest"); }
+    public void postProcess(net.minecraft.world.level.WorldGenLevel level, net.minecraft.world.level.StructureManager manager, net.minecraft.world.level.chunk.ChunkGenerator generator, RandomSource random, BoundingBox box, net.minecraft.world.level.ChunkPos chunkPos, BlockPos pos) {
+        // TODO: template placement
+    }
+}
--- src/main/java/mods/defeatedcrow/common/world/village/ComponentVillageWarehouse.java
+++ src/main/java/mods/defeatedcrow/common/world/village/ComponentVillageWarehouse.java
@@ -1,2 +1,28 @@
 package mods.defeatedcrow.common.world.village;
-public class ComponentVillageWarehouse {}
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+
+/**
+ * 1.20.1 ComponentVillageWarehouse - warehouse structure (was StructureVillagePieces.Village).
+ */
+public class ComponentVillageWarehouse {
+
+    private boolean hasMadeChest;
+
+    public ComponentVillageWarehouse() {}
+
+    public boolean addComponentParts(ServerLevelAccessor level, RandomSource random, BoundingBox box) {
+        // TODO: place NBT template data/defeatedcrow/structures/village_warehouse.nbt
+        return true;
+    }
+
+    protected void addAdditionalSaveData(CompoundTag tag) { tag.putBoolean("Chest", hasMadeChest); }
+    protected void readAdditionalSaveData(CompoundTag tag) { hasMadeChest = tag.getBoolean("Chest"); }
+    public void postProcess(net.minecraft.world.level.WorldGenLevel level, net.minecraft.world.level.StructureManager manager, net.minecraft.world.level.chunk.ChunkGenerator generator, RandomSource random, BoundingBox box, net.minecraft.world.level.ChunkPos chunkPos, BlockPos pos) {
+        // TODO: template placement
+    }
+}
